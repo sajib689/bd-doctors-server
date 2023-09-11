2f6a48153dbeb2b763545e211307e515984cae69
sajib689 <jibonraja11@gmail.com>
2023-09-11 10:43:57 +0600

    await issu fix

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
     const bddoctorsCollection = client.db('bdDoctor').collection('services')
     const appointmentsCollection = client.db('bdDoctor').collection('appointment')
     // get services from the database
@@ -96,7 +96,7 @@ async function run() {
         res.send(result)
       })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+     client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
