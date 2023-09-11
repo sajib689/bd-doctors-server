e34c174e5a450db944db839df43356b4f88dec7a
sajib689 <jibonraja11@gmail.com>
2023-09-11 22:19:50 +0600

    u

--- index.js
+++ index.js
@@ -45,7 +45,6 @@ async function run() {
   //  jwt
   app.post('/jwt', (req, res) => {
     const user = req.body
-    console.log(user)
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
       expiresIn: '1h'
      
@@ -80,7 +79,7 @@ async function run() {
     // get appiontment data from database
     app.get('/appointment',verifyJWT, async(req, res) => {
       const decoded = req.decoded
-      if(decoded.email !== req.query.email) {
+      if(decoded.email !== req.query?.email) {
         return res.status(403).send({error: true, message: 'forbidden access token'})
       }
       let query = {}
